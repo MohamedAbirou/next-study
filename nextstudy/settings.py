95d0a30945980b388f47d730544c47a82a4c825b
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-03 02:37:26 +0100

    updated settings.py

--- nextstudy/settings.py
+++ nextstudy/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.environ.get("STATIC_URL")
+STATIC_URL = '/static/styles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
