3c140c3256ee99f9d8b9abcd6869c14052661de0
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-03 02:23:55 +0100

    updated settings.py

--- nextstudy/settings.py
+++ nextstudy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 
 
 # Application definition
