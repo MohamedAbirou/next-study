0c356a2ca4baa1529b9f20d7ad5daca141ffd866
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-03 02:01:35 +0100

    removed venv

--- venv/Lib/site-packages/vercel/__init__.py
+++
@@ -1,4 +0,0 @@
-from vercel.resources import *
-
-api_token = None
-team_id = None
--- venv/Lib/site-packages/vercel/exceptions.py
+++
@@ -1,4 +0,0 @@
-class VercelError(Exception):
-    def __init__(self, code, message):
-        self.code = code
-        self.message = message
